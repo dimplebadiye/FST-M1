f77bdf6353deb41125ef53cbbae971cfa1e43dad
Dimple <68469052+dimplebadiye@users.noreply.github.com>
2024-05-24 16:20:36 +0530

    Add files via upload

---
+++ Selenium/Project/BaseClass.java
@@ -0,0 +1,41 @@
+package project;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+
+public class BaseClass {
+
+    public static WebDriver driver;
+    public static WebDriverWait wait;
+
+    public void setUp() {
+
+        // Set up Firefox driver
+        WebDriverManager.firefoxdriver().setup();
+
+        // Create a new instance of the Firefox driver
+        driver = new FirefoxDriver();
+
+        //Wait
+        wait = new WebDriverWait(driver, Duration.ofSeconds(30));
+
+        // Open the page
+        driver.get("http://alchemy.hguy.co/orangehrm");
+    }
+
+    public void login(){
+        driver.findElement(By.id("txtUsername")).sendKeys("orange");
+        driver.findElement(By.id("txtPassword")).sendKeys("orangepassword123");
+        driver.findElement(By.id("btnLogin")).click();
+    }
+
+    public void tearDown(){
+        // Close the browser
+        driver.close();
+    }
+}
